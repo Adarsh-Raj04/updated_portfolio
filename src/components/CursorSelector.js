ecd244f2eb0e63efea3da7a33301ecf00099526e
Adarsh-Raj04 <adarsh.raj.2004@outlook.com>
2026-02-14 23:34:23 +0530

    fix the import issue

--- src/components/CursorSelector.js
+++ src/components/CursorSelector.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
-
 export default function CursorSelector() {
-    // Component disabled - using default browser cursor
-    return null;
+  // Component disabled - using default browser cursor
+  return null;
 }
